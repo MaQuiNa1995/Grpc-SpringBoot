aa0aa76d5c031ebf74ccea720e3fd6321373f7f2
MaQuiNa1995 <maquina1995@gmail.com>
2021-11-09 21:51:17 +0100

    Adicion de plugins maven y cambio en el codigo del servidor

---
+++ src/main/java/com/github/maquina1995/grpc/server/HelloWorldServerImpl.java
@@ -0,0 +1,34 @@
+
+package com.github.maquina1995.grpc.server;
+
+import org.lognet.springboot.grpc.GRpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.maquina1995.grpc.Greeting;
+import com.github.maquina1995.grpc.HelloWorldServiceGrpc.HelloWorldServiceImplBase;
+import com.github.maquina1995.grpc.Person;
+
+import io.grpc.stub.StreamObserver;
+
+@GRpcService
+public class HelloWorldServerImpl extends HelloWorldServiceImplBase {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldServerImpl.class);
+
+	@Override
+	public void sayHello(Person request, StreamObserver<Greeting> responseObserver) {
+		LOGGER.info("server received {}", request);
+
+		String message = "Hello " + request.getFirstName() + " " + request.getLastName() + "!";
+
+		Greeting greeting = Greeting.newBuilder()
+		        .setMessage(message)
+		        .build();
+
+		LOGGER.info("server responded {}", greeting);
+
+		responseObserver.onNext(greeting);
+		responseObserver.onCompleted();
+	}
+}
